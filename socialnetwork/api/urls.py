5d0910e202b0fc17e9c43a16f56546a28f01788c
asthaporwal2898@gmail.com <asthaporwal@Asthas-MacBook-Pro.local>
2024-06-02 18:28:03 +0530

    modifying postman collection file

--- socialnetwork/api/urls.py
+++ socialnetwork/api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('search/', SearchUserView.as_view(), name='search'),
     path('friend-request/', FriendRequestView.as_view(), name='friend-request'),
-    path('friend-request/', ManageFriendRequestView.as_view(), name='manage-friend-request'),
+    path('friend-request/<int:id>/<str:action>/', ManageFriendRequestView.as_view(), name='manage-friend-request'),
     path('friends/', ListFriendsView.as_view(), name='list-friends'),
     path('pending-requests/', ListPendingRequestsView.as_view(), name='pending-requests'),
 ]
